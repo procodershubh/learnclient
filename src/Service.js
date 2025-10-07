960c6d7c1972868227aff82b26ab499e9e0f2605
procodershubh <shubhamkumar84545556@gmail.com>
2025-10-07 19:50:44 +0530

    aa

--- src/Service.js
+++ src/Service.js
@@ -1,10 +1,10 @@
-// // live server
+// live server
 export const backendurl = "https://learnbackend-604p.onrender.com";
 
 
 
 
-// //local server
+//local server
 
 
 // export const backendurl = "http://localhost:5782";
